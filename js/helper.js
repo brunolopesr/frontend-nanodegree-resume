2a10e0030f487e44a334624d6e2e2b74a77c1c8a
Bruno Lopes <bruno_lopes@live.com>
2017-03-29 17:55:58 -0300

    Projeto concluído

--- js/helper.js
+++ js/helper.js
@@ -13,20 +13,20 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">celular</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">endereço</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3" class="flex-column">Conhecimentos:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -49,7 +49,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3>Cursos Online</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,211 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    name: "Bruno Lopes",
+    role: "Desenvolvedor Web Front-End",
+    contacts: {
+        mobile: "(11) 98761-0579",
+        email: "bruno_lopes@live.com",
+        github: "https://github.com/brunolopesr",
+        location: "Estrada do M Boi Mirim, 2912 - Jardim Regina, São Paulo - SP"
+    },
+    welcomeMessage: "Este é o projeto de curriculo do Nanodegree",
+    skills: ["HTML5", "CSS3", "JavaScript", "jQuery", "Design Responsivo", "PHP"],
+    biopic: "images/bruno.jpg",
+    display: function () {
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(formattedName);
+
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $(formattedRole).insertAfter("#name");
+
+        for(var contactType in bio.contacts) {
+            var contact = bio.contacts[contactType];
+
+            var formattedTag = eval("HTML" + contactType);
+            formattedTag = formattedTag.replace("%data%", contact);
+
+            $("#topContacts").append(formattedTag);
+            $("#footerContacts").append(formattedTag);
+        }
+
+        var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+        $("#header").append(formattedPic);
+
+        var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        $("#header").append(formattedMsg);
+
+        if(bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+            bio.skills.forEach(function(skill) {
+                var formattedSkill = HTMLskills.replace("%data%", skill);
+                $("#skills").append(formattedSkill);
+            })
+        }
+    }
+}
+
+var work = {
+    jobs: [
+        {
+            employer: "Locaweb",
+            title: "Analista de Suporte",
+            location: "R. Itapaiúna, 2434 - Jardim Morumbi, São Paulo - SP",
+            dates: "11/2013 - 10/2014",
+            description: "Fornecer suporte técnico para clientes contratantes dos serviços de Hospedagem da Locaweb"
+        },
+        {
+            employer: "Colégio Benjamin Constant",
+            title: "Especialista em T.I",
+            location: "Rua Eça de Queiroz, 75 - Vila Mariana, São Paulo - SP",
+            dates: "12/2015 até hoje",
+            description: "Prestar consultoria e suporte técnico em infraestrutura de rede, hardware e software de T.I"
+        }
+    ],
+    display: function() {
+        var formattedTag;
+        if(work.jobs.length > 0) {
+            work.jobs.forEach(function(job) {
+                var workEntry = $(HTMLworkStart);
+
+                var jobEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+                var jobTitle = HTMLworkTitle.replace("%data%", job.title);
+                var jobDate = HTMLworkDates.replace("%data%", job.dates);
+                var jobLocation = HTMLworkLocation.replace("%data%", job.location);
+                var jobDescription = HTMLworkDescription.replace("%data%", job.description);
+
+                workEntry.append(jobEmployer + jobTitle);
+                workEntry.append(jobDate);
+                workEntry.append(jobLocation);
+                workEntry.append(jobDescription);
+
+
+
+                $("#workExperience").append(workEntry);
+            })
+        }
+    }
+}
+
+var projects = {
+    projects: [
+        {
+            title: "Projeto de Exemplo",
+            dates: "01/2016 até 08/2016",
+            description: "Projeto que não existe, mas que eu coloquei aqui para exemplo no curriculum",
+            images: ["images/197x148.gif"]
+        }
+    ],
+    display: function() {
+        if(projects.projects.length > 0) {
+            projects.projects.forEach(function(project) {
+                var projectEntry = $(HTMLprojectStart);
+
+                var projectTitle = HTMLprojectTitle.replace("%data%", project.title);
+                projectEntry.append(projectTitle);
+
+                var projectDates = HTMLprojectDates.replace("%data%", project.dates);
+                projectEntry.append(projectDates);
+
+                var projectDescription = HTMLprojectDescription.replace("%data%", project.description);
+                projectEntry.append(projectDescription);
+
+                if(project.images.length > 0) {
+                    for(var j = 0; j < project.images.length; j++) {
+                        var projectImage = HTMLprojectImage.replace("%data%", project.images[j]);
+                        projectEntry.append(projectImage);
+                    }
+                }
+
+                $("#projects").append(projectEntry);
+            })
+        }
+    }
+}
+
+var education = {
+    schools: [
+        {
+            name: "Centro Universitário IBTA",
+            location: "Av. Paulista, 302 - Bela Vista, São Paulo - SP",
+            degree: "Bacharelado em Ciências da Computação",
+            majors: ["CS"],
+            dates: "2014-2018",
+        },
+        {
+            name: "ETEC Zona Sul",
+            location: "Rua Frederico Grotte, 322 - Jardim Vergueiro, São Paulo - SP",
+            degree: "Ensino Médio",
+            majors: ["CS"],
+            dates: "2008-2010",
+        }
+    ],
+    onlineCourses: [
+        {
+            title: "Front-End Web Developer Nanodegree",
+            school: "Udacity",
+            dates: "2017",
+            url: "http://udacity.com.br/"
+        }
+    ],
+    display: function() {
+        if(education.schools.length > 0) {
+            education.schools.forEach(function(school) {
+                var schoolEntry = $(HTMLschoolStart);
+
+                var schoolName = HTMLschoolName.replace("%data%", school.name);
+                var schoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+
+                schoolEntry.append(schoolName + schoolDegree);
+
+                var schoolDates = HTMLschoolDates.replace("%data%", school.dates);
+                schoolEntry.append(schoolDates);
+
+                var schoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+                schoolEntry.append(schoolLocation);
+
+                /* Não tenho a mínima ideia do que seja um Major na Educação Brasileira */
+                school.majors.forEach(function(element) {
+                    var schoolMajor = HTMLschoolMajor.replace("%data%", element);
+                    schoolEntry.append(schoolMajor);
+                })
+
+                $("#education").append(schoolEntry);
+            })
+        }
+
+        if(education.onlineCourses.length > 0) {
+            $("#education").append(HTMLonlineClasses);
+            education.onlineCourses.forEach(function(course) {
+                var schoolEntry = $(HTMLschoolStart);
+
+                var courseTitle = HTMLonlineTitle.replace("%data%", course.title);
+                var courseSchool = HTMLonlineSchool.replace("%data%", course.school);
+                schoolEntry.append(courseTitle + courseSchool);
+
+                var courseDates = HTMLonlineDates.replace("%data%", course.dates);
+                schoolEntry.append(courseDates);
+
+                var courseURL = HTMLonlineURL.replace("%data%", course.url);
+                schoolEntry.append(courseURL);
+
+                $("#education").append(schoolEntry);
+            })
+        }
+    }
+};
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+// Google Maps
+
+$("#mapDiv").append(googleMap);
+
+// Traduções de algumas coisas
+
+$("#lets-connect").find("h2").text("Entre em contato");
+$("#workExperience").find("h2").text("Experiência");
+$("#projects").find("h2").text("Projetos");
+$("#education").find("h2").text("Educação");
+$("#mapDiv").find("h2").text("Onde morei e trabalhei");
